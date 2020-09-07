0f683240734eebd4642023f9325c239becf741b3
zouyang <zouyangchn@qq.com>
2020-09-07 15:33:50 +0800

    初始化

---
+++ config-server/src/test/java/com/zouyang/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zouyang.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
